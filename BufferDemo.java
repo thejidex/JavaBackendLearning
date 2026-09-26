7235af93bc3f674138137c35df50b51632a81907
归虚梦演 <3085294809@qq.com>
2026-09-26 11:46:55 +0800

    transferTo  Demo

--- BufferDemo.java
+++ BufferDemo.java
@@ -1,41 +1,16 @@
 package Socket;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class BufferDemo {
-    public static void main(String[] args) {
-        // 创建一个缓冲区
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-
-// 看一下初始时4个核心变量的值
-        System.out.println("初始时-->limit--->"+byteBuffer.limit());
-        System.out.println("初始时-->position--->"+byteBuffer.position());
-        System.out.println("初始时-->capacity--->"+byteBuffer.capacity());
-        System.out.println("初始时-->mark--->" + byteBuffer.mark());
-
-        System.out.println("--------------------------------------");
-
-// 添加一些数据到缓冲区中
-        String s = "沉默王二";
-        byteBuffer.put(s.getBytes());
-
-// 看一下初始时4个核心变量的值
-        System.out.println("put完之后-->limit--->"+byteBuffer.limit());
-        System.out.println("put完之后-->position--->"+byteBuffer.position());
-        System.out.println("put完之后-->capacity--->"+byteBuffer.capacity());
-        System.out.println("put完之后-->mark--->" + byteBuffer.mark());
-
-        // flip()方法
-        byteBuffer.flip();
-        System.out.println("flip()方法之后-->limit--->"+byteBuffer.limit());
-        System.out.println("flip()方法之后-->position--->"+byteBuffer.position());
-        System.out.println("flip()方法之后-->capacity--->"+byteBuffer.capacity());
-        System.out.println("flip()方法之后-->mark--->" + byteBuffer.mark());
-        // 创建一个limit()大小的字节数组(因为就只有limit这么多个数据可读)
-        byte[] bytes = new byte[byteBuffer.limit()];
-// 将读取的数据装进我们的字节数组中
-        byteBuffer.get(bytes);
-// 输出数据
-        System.out.println(new String(bytes, 0, bytes.length));
+    public static void main(String[] args) throws IOException {
+        FileChannel source = FileChannel.open(Paths.get("a.txt"), StandardOpenOption.READ);
+        FileChannel des = FileChannel.open(Paths.get("b.txt"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        source.transferTo(0, source.size(), des);
     }
 }
