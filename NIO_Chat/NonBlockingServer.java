500d344eb56b68ba7b1a7cc155bce1227b525fa3
归虚梦演 <3085294809@qq.com>
2026-09-26 22:23:16 +0800

    自己尝试手写，只写了连接部分，后面还没写。今天不写了，我刷抖音去了。

--- NIO_Chat/NonBlockingServer.java
+++ NIO_Chat/NonBlockingServer.java
@@ -14,48 +14,24 @@ import java.util.Set;
 
 public class NonBlockingServer {
     public static void main(String[] args) throws IOException {
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
         serverSocketChannel.socket().bind(new InetSocketAddress(8888));
         serverSocketChannel.configureBlocking(false);
+        Selector selector=Selector.open();
+        serverSocketChannel.register(selector,SelectionKey.OP_ACCEPT);
 
-        // 创建选择器
-        Selector selector = Selector.open();
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        while (true) {
+        while(true){
             selector.select();
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iterator = selectedKeys.iterator();
-            while (iterator.hasNext()) {
-                SelectionKey key = iterator.next();
+            Iterator<SelectionKey>iterator=selector.selectedKeys().iterator();
+
+            while(iterator.hasNext()){
+                SelectionKey key=iterator.next();
                 iterator.remove();
-
-                if (key.isAcceptable()) {
-                    // 获取客户端连接
-                    SocketChannel socketChannel = serverSocketChannel.accept();
-                    socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                }
-                if (key.isReadable()) {
-                    SocketChannel socketCHannel = (SocketChannel) key.channel();
-                    ByteBuffer buffer = ByteBuffer.allocate(1024);
-                    int read = socketCHannel.read(buffer);
-                    if (read != -1) {
-                        buffer.flip();
-                        System.out.println(StandardCharsets.UTF_8.decode(buffer));
-                        buffer.clear();
-                    } else {
-                        System.out.println("客户端已断开");
-                        key.cancel();
-                        socketCHannel.close();
-                        continue;
-                    }
-                }
-
-                if (key.isWritable()) {
-                    SocketChannel client = (SocketChannel) key.channel();
-                    ByteBuffer buffer = ByteBuffer.wrap("你好，客户端".getBytes());
-                    client.write(buffer);
+                if(key.isAcceptable()){
+                    System.out.println("客户端已连接");
+                    SocketChannel client=serverSocketChannel.accept();
+                    client.configureBlocking(false);
+                    client.register(selector,SelectionKey.OP_READ);
                 }
             }
         }
